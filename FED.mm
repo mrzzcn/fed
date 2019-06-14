2d08731fcb93d15d850d3ff66f5c02da84da4d63
Jack <mrzzcn@gmail.com>
2019-06-14 22:22:55 +0800

    V1

---
+++ FED.mm
@@ -0,0 +1,135 @@
+<map>
+		<node ID="root" TEXT="FED">		<node TEXT="JS" ID="13416b564bd134055" STYLE="bubble" POSITION="right">
+		<node TEXT="文法" ID="37816b564bf9ed068" STYLE="fork">
+		<node TEXT="词法" ID="8516b564c7672032" STYLE="fork">
+		</node>
+		<node TEXT="语法" ID="4616b564c810718c" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="语义" ID="8216b564c281704f" STYLE="fork">
+		</node>
+		<node TEXT="运行时" ID="17516b564c3227109" STYLE="fork">
+		<node TEXT="类型/数据结构" ID="16316b564c9310016" STYLE="fork">
+		<node TEXT="类型" ID="2e416b564f9060197" STYLE="fork">
+		<node TEXT="number/Number" ID="34e16b5651a695169" STYLE="fork">
+		</node>
+		<node TEXT="string/String" ID="32c16b5651ce7a0ad" STYLE="fork">
+		</node>
+		<node TEXT="boolean/Boolean" ID="13916b5651d942022" STYLE="fork">
+		</node>
+		<node TEXT="undefined" ID="23916b5651e38a10f" STYLE="fork">
+		</node>
+		<node TEXT="Null" ID="3b516b5652bdd0096" STYLE="fork">
+		</node>
+		<node TEXT="Symbol" ID="35a16b565209cc055" STYLE="fork">
+		</node>
+		<node TEXT="Object" ID="e416b56521b2d11e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="实例" ID="2c016b564fb3af0f9" STYLE="fork">
+		<node TEXT="应用和机制" ID="1e216b564fcf4e121" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		<node TEXT="执行过程（算法）" ID="27e16b564c9e180ff" STYLE="fork">
+		<node TEXT="事件循环" ID="22616b564febff0d" STYLE="fork">
+		</node>
+		<node TEXT="微任务的执行" ID="17416b564ff9ef146" STYLE="fork">
+		</node>
+		<node TEXT="函数的执行" ID="31416b56503ee811" STYLE="fork">
+		</node>
+		<node TEXT="语句的执行" ID="5116b565050c005c" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="HTML" ID="3c316b564be4cf0a6" STYLE="bubble" POSITION="right">
+		<node TEXT="元素" ID="37616b5653a0b1046" STYLE="fork">
+		<node TEXT="文档元信息" ID="716b5653c16e05c" STYLE="fork">
+		</node>
+		<node TEXT="语义相关" ID="15d16b5653e2a7155" STYLE="fork">
+		</node>
+		<node TEXT="链接" ID="26716b5653f80e044" STYLE="fork">
+		</node>
+		<node TEXT="替换型元素" ID="28016b56540e0e01d" STYLE="fork">
+		</node>
+		<node TEXT="表单" ID="1d16b56543de716c" STYLE="fork">
+		</node>
+		<node TEXT="表格" ID="30216b565447f607a" STYLE="fork">
+		</node>
+		<node TEXT="总集" ID="2ac16b56544f48091" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="语言" ID="37916b565474c803" STYLE="fork">
+		<node TEXT="实体" ID="22516b565482a10e3" STYLE="fork">
+		</node>
+		<node TEXT="命名空间" ID="31916b56548c880c8" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="补充标准" ID="3ab16b5654a1b80d8" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="CSS" ID="3b016b564bee8d016" STYLE="bubble" POSITION="right">
+		<node TEXT="语言" ID="26c16b5654d0610dc" STYLE="fork">
+		<node TEXT="@rule" ID="1d716b5654e6b9059" STYLE="fork">
+		</node>
+		<node TEXT="选择器" ID="2f116b56550260168" STYLE="fork">
+		</node>
+		<node TEXT="单位" ID="17316b5655186a125" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="功能" ID="36916b5654dd4815c" STYLE="fork">
+		<node TEXT="布局" ID="9516b565520d1137" STYLE="fork">
+		<node TEXT="正常流" ID="5b16b56555feb11e" STYLE="fork">
+		</node>
+		<node TEXT="弹性布局" ID="33416b56557a210bf" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="绘制" ID="2e216b56552ce1158" STYLE="fork">
+		<node TEXT="颜色和形状" ID="38616b5655877204" STYLE="fork">
+		</node>
+		<node TEXT="文字" ID="9b16b5655a09e0d3" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="交互" ID="1ed16b5655536c07e" STYLE="fork">
+		<node TEXT="动画" ID="17b16b5655b0e214c" STYLE="fork">
+		</node>
+		<node TEXT="其他" ID="4116b5655bb14069" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="浏览器实现原理" ID="1916b5656ff2802c" STYLE="bubble" POSITION="left">
+		<node TEXT="解析" ID="28a16b5657281f04c" STYLE="fork">
+		</node>
+		<node TEXT="构建DOM树" ID="26516b56573605138" STYLE="fork">
+		</node>
+		<node TEXT="计算CSS" ID="37616b5657552d018" STYLE="fork">
+		</node>
+		<node TEXT="渲染、合成与绘制" ID="25316b56576535039" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="API" ID="d116b56571dd60ea" STYLE="bubble" POSITION="left">
+		<node TEXT="DOM" ID="13d16b5657a5ce175" STYLE="fork">
+		</node>
+		<node TEXT="CSSOM" ID="1e216b5657b46f1" STYLE="fork">
+		</node>
+		<node TEXT="事件" ID="23416b5657c17d0a4" STYLE="fork">
+		</node>
+		<node TEXT="API总集合" ID="1ae16b5657cc4812a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="前端工程实践" ID="25216b56581f890f7" STYLE="bubble" POSITION="left">
+		<node TEXT="性能" ID="2f516b565836a901a" STYLE="fork">
+		</node>
+		<node TEXT="工具链" ID="33916b565840ee01e" STYLE="fork">
+		</node>
+		<node TEXT="持续集成" ID="7f16b5658545705a" STYLE="fork">
+		</node>
+		<node TEXT="搭建系统" ID="3b416b5658652f176" STYLE="fork">
+		</node>
+		<node TEXT="架构与基础库" ID="1ed16b56587e6108c" STYLE="fork">
+		</node>
+		</node>
+</node>
+</map>
---
+++ README.md
@@ -0,0 +1,5 @@
+# 重学前端知识图谱
+
+[FreeMind](https://github.com/mrzzcn/fed/raw/master/FED.mm)
+
+![重学前端知识图谱](https://github.com/mrzzcn/fed/raw/master/FED.png)
